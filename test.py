c7d631cb3ba782bfb8d342ccade7f5b6deb3fe94
zgm <2422790045@qq.com>
2023-02-25 21:11:51 +0800

    dsfad

--- test.py
+++ test.py
@@ -1,3 +1,23 @@
 import time
-print(time.time())
-print("-----------")
+
+
+
+def change_time_to_beijin_time(str_time):
+    year_month_day = str_time.split('T')[0]
+    hour_time = (str_time.split('T')[1]).split('+')[0]
+    time_ =  f'{year_month_day} {hour_time}'
+    t_time = time.localtime(int(time.mktime(time.strptime(time_, "%Y-%m-%d %H:%M:%S"))) + 3600 * 1)
+    beijin_time = time.strftime("%Y-%m-%d %H:%M:%S", t_time)
+    return beijin_time
+
+
+if __name__ == '__main__':
+    # print(get_date_str("2023-01-05T10:00:00+01:00"))
+    ss="""# https://magazine.interencheres.com/actualites/
+    # https://magazine.interencheres.com/tendances/
+    # https://magazine.interencheres.com/coulisses-des-encheres/
+    # https://magazine.interencheres.com/art-mobilier/
+    # https://magazine.interencheres.com/vehicules/
+    # https://magazine.interencheres.com/materiels-professionnels/"""
+    ''.replace("# ",'')
+    print([s.strip().replace("# ",'') for s in ss.split("\n")])
